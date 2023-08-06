8649c174b0104ea0d75ea46116fce18c0e716396
Manvi <98814117+DizManvi@users.noreply.github.com>
2023-08-07 00:48:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm'
+
+function App() {
+  return (
+    <>
+    <Navbar title="TextUtils"  about="AboutMe" />
+    <div className="container my-3">
+    <TextForm  heading="Enter the text to analyze"/>
+    </div>
+    </>
+  
+    
+  );
+}
+
+export default App;
